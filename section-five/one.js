353b38bf85f802c9e8274bca7a9ddb16d20b401c
Darragh Cullinan <darraghcullinan@Darraghs-MacBook-Air.local>
2021-04-24 10:55:10 +0100

    push up work from tuesday and add link to index for one.html

--- section-five/one.js
+++ section-five/one.js
@@ -7,3 +7,19 @@ document.write(now)
 function hi() { 
     document.write("You have been Called")
 }
+
+function changeParagraph() {
+    counter = counter + 1 
+    document.getElementById("change").innerHTML= names;
+    document.getElementById("headerChange").innerHTML= counter;
+}
+
+var number = 10;
+var names = "Darragh";
+var onOff = true;
+var counter = 0;
+var x, y, z;
+var a = 1, b = 2, c = 3, d = 4;
+var car, speed = 100, name = "Awesome", color = "red";
+
+
