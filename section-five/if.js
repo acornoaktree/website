44b4a3d1a6b82733db7baeb10f3f72b2e604f122
Darragh Cullinan <darraghcullinan@Darraghs-MacBook-Air.local>
2021-05-15 17:46:35 +0100

    commit work

---
+++ section-five/if.js
@@ -0,0 +1,9 @@
+function myAwesomeFunction()
+{
+    if (a === 1)
+    {
+        document.getElementById('p').innerHTML = "paragraph CHANGED!";
+    }
+}
+
+var a = 3;
--- section-five/two.js
+++ section-five/two.js
@@ -1,17 +1,32 @@
-var income =  12
-var bankAccount = 0
-var debt = 5
+var income =  3500
+var bankAccount = 2000
+var debt = 0
+var rent = 800
+var car = 200
+var food = 300
+var entertainment = 200
+
+debt = rent + car + food + entertainment;
 
 function myAwesomeFunction(){
     bankAccount = bankAccount + income;
 
-    document.getElementById("headerChange").innerHTML = "This is how much money you owe: $" + debt
+    document.getElementById("rent").innerHTML = "Rent per month = $" + rent + " Total rent $" + rent;
+    document.getElementById("car").innerHTML = "Total car: $" + car;
+    document.getElementById("food").innerHTML = "Total food: $" + food;
+    document.getElementById("entertainment").innerHTML = "Total entertainment: $" + entertainment;
     document.getElementById("result").innerHTML = "This is how much money you have left: $" + (bankAccount - debt)
     bankAccount = bankAccount - debt;
     document.getElementById("change").innerHTML = "Bank account: $" + bankAccount
+    rent = rent + 800;
+    car = car + 200;
+    food = food + 300;
+    entertainment = entertainment + 200;
 }
 
 function myAwesomeFunction2(){
+    bankAccount = bankAccount + 10;
+    document.getElementById("headerChange").innerHTML = "This is how much money you owe: $" + debt
     document.getElementById("income").innerHTML = "This is your total income: $" + income
 }
 
