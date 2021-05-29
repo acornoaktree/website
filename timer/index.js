d2db2e7ee326c638156e0ec6f3abccc95e45c0b8
Darragh Cullinan <darraghcullinan@Darraghs-MacBook-Air.local>
2021-05-29 09:40:28 +0100

    add timer to index

--- timer/index.js
+++ timer/index.js
@@ -1,5 +1,24 @@
-function start()
+var startTime 
+var intervalId
+var running = false
+function start(){
+    if (running === false ){
+      startTime = new Date()
+      intervalId = setInterval(updateTimer, 100)
+      running = true
+    }
+    }
 
 
 
-function stop()
+function stop(){
+    clearInterval(intervalId)
+    running = false
+}
+
+function updateTimer(){
+    var timer = document.getElementById("timer")
+    var now = new Date()
+    var millisecondsPast = now.getTime() - startTime.getTime()
+    timer.innerHTML = millisecondsPast / 1000
+}
