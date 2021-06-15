e3a79c5d9786203a75f033a8bcc18fb7047cd3a6
Darragh Cullinan <darraghcullinan@Darraghs-MacBook-Air.local>
2021-06-15 19:20:15 +0100

    perform calculation on a tip change

--- tip-calculator/index.js
+++ tip-calculator/index.js
@@ -1,8 +1,6 @@
 function calculateTotalBill(){
     const bill = document.getElementById('bill').value
     const tip = document.getElementById('tip').value
-    console.log(bill)
-    console.log(tip)
     const totalBill = document.getElementById('total-bill')
     totalBill.value = (parseInt(bill) / 100) * parseInt(tip) + parseInt(bill)
 }
